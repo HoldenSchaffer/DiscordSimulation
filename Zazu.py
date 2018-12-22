3049c2e3d8697ed651eba57fe0608dfd62c1d96d
Holden Schaffer <hschaffer7@gatech.edu>
2018-12-21 23:29:01 -0400

    Token test

---
+++ Zazu.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+
+token = open('token.txt', r).read()
+print(token)
+client = discord.Client()
+
+bot =commands.Bot(command_prefix="zazu ")
+@bot.event
+async def on_ready():
+    print ("Ready when you are xd")
+    print ("I am running on "+ bot.user.name)
+    print ("with the ID: " + bot.user.id)
+
+@bot.command(pass_context=True)
+async def ping(ctx):
+    await bot.say("AAAAAAAAAAAAAAAAAAAAA IM ON FIRE SOMEONE PUT ME OUT")
